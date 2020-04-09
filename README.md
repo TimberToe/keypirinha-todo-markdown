79e501198ab92697b0833a41d10f9127683b692f
Anton Malmrot <anton.malmrot@novasoftware.se>
2020-04-09 09:27:38 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,106 @@
+# Keypirinha Plugin: todo-markdown
+
+**NOTE: I have barely started development and it is far from working**
+
+This is todo-markdown, a plugin for the
+[Keypirinha](http://keypirinha.com) launcher.
+
+A plugin to easily add/finish/list todos from a Markdown file.
+
+I use a markdown file to jolt down small todos that don't fit in our ticket system.
+Since I use Keypirinha for everything else, why not as as shortcut to my todo list!
+
+Since it is an markdown file it is easy to sync between computers through your cloud storage.
+It is easy to read and convert to another format if needed
+
+## Download
+
+**TODO:** indicate where the latest `.keypirinha-package` file can be
+downloaded. For example a URL to the `releases` list like:
+https://github.com/USERNAME/keypirinha-PACKAGE/releases
+
+
+## Install
+
+Once the `todo-markdown.keypirinha-package` file is installed,
+move it to the `InstalledPackage` folder located at:
+
+* `Keypirinha\portable\Profile\InstalledPackages` in **Portable mode**
+* **Or** `%APPDATA%\Keypirinha\InstalledPackages` in **Installed mode** (the
+  final path would look like
+  `C:\Users\%USERNAME%\AppData\Roaming\Keypirinha\InstalledPackages`)
+
+
+## Usage
+
+
+You will be able to:
+ - Add todos
+ - Finish todos
+ - List top todos
+ - Open the markdown file that stores the todos
+
+**TODO:** list the items, if any, inserted to the Catalog by the plugin(s) of
+this package. Some plugins only make suggestions, in which case a description of
+what kind of suggestions are to be expected by the user may help.
+
+### Gerkin
+
+Feature: Manage Todos in the Markdown file
+ - Example: Load todos on start up
+ - Example: Load todos on open of application?
+ - Example: No file found
+ - Feature: Parse Markdown file
+   - Example: Malformed file
+   - Example: Empty file
+   - Example: HUGE file??
+Feature: Managing Todos
+ - Feature: Add todo
+ - Feature: List todo
+ - Feature: Finish todo
+ - Feature: Delete todo
+ - Feature: Load more?
+Feature: Copy and delete (Assign to another)
+Feature: Open markdown file
+Feature: Change settings
+ - Example: Change path to Markdown file
+ - Example: Change keywords?
+ - 
+
+
+## Change Log
+
+**TODO:** describe notable changes for each release. Below is a template for
+version 1.0.
+
+### v0.1
+
+* Added foo item
+* Fixed bug that was doing bad things when item "bar" was selected
+
+
+## License
+
+**TODO:** recommended section. Below is an example that goes with the default
+LICENSE file (MIT license). Do not forget to add your name in the `LICENSE`
+file!
+
+This package is distributed under the terms of the MIT license.
+
+
+## Credits
+
+**TODO:** optional section.
+
+
+## Contribute
+
+**TODO:** optional section. Below is a template example, based on the one found
+in Keypirinha's Packages repository.
+
+1. Check for open issues or open a fresh issue to start a discussion around a
+   feature idea or a bug.
+2. Fork this repository on GitHub to start making your changes to the **dev**
+   branch.
+3. Send a pull request.
+4. Add yourself to the *Contributors* section below (or create it if needed)!
---
+++ src/todo-markdown.py
@@ -0,0 +1,210 @@
+# Keypirinha launcher (keypirinha.com)
+
+# 
+#    In on_start I should 
+#     - Read config
+#
+#    In on_catalog I should
+#     - Read the Markdown file? 
+#       # Might be heavy on each on_catalog.  Needs to be more often than on_start though...
+#     - Add to the catalog:
+#       - Todo
+#         - Add
+#         - Finish
+#         - List
+#         - Open file
+#
+#   In on_events I should:
+#    - Scan the markdown file since the config has changed
+#
+#   In on_suggest under:
+#    - "Todo" I should
+#      - List the top todos
+#    - "Add" I should search todos so that I can see if I'm about to add a double todo
+#       - The top item should always be an "add new todo" that will be executed on enter
+#    - "Finish" I should search todos to find the one I want to finich
+#    - "List" I should directly show a list of *all* todos (if possible)
+#    - "Open file" I should open the markdown file in default editor
+#   
+#   In on_execute I should
+#    - If under "Finish" I should finish the todo
+#    - If under "Add" I should only add a new todo on the "add new todo" otherwise to nothing
+#    - If under "List" I should do nothing on enter
+#    - If under "actions for an item" these actions should be visible:
+#        - Delete
+#        - Finish
+#        - (Keypirinhas normal ones should not be listed)
+
+
+
+
+
+#   TODO: Checkout MovieDB plugin! It has manage to get about the structure I want
+
+
+import keypirinha as kp
+import keypirinha_util as kpu
+import keypirinha_net as kpnet
+
+class todo_markdown(kp.Plugin):
+    """
+    One-line description of your plugin.
+
+    This block is a longer and more detailed description of your plugin that may
+    span on several lines, albeit not being required by the application.
+
+    You may have several plugins defined in this module. It can be useful to
+    logically separate the features of your package. All your plugin classes
+    will be instantiated by Keypirinha as long as they are derived directly or
+    indirectly from :py:class:`keypirinha.Plugin` (aliased ``kp.Plugin`` here).
+
+    In case you want to have a base class for your plugins, you must prefix its
+    name with an underscore (``_``) to indicate Keypirinha it is not meant to be
+    instantiated directly.
+
+    In rare cases, you may need an even more powerful way of telling Keypirinha
+    what classes to instantiate: the ``__keypirinha_plugins__`` global variable
+    may be declared in this module. It can be either an iterable of class
+    objects derived from :py:class:`keypirinha.Plugin`; or, even more dynamic,
+    it can be a callable that returns an iterable of class objects. Check out
+    the ``StressTest`` example from the SDK for an example.
+
+    Up to 100 plugins are supported per module.
+
+    More detailed documentation at: http://keypirinha.com/api/plugin.html
+
+    """
+    
+    TODO_CAT = kp.ItemCategory.USER_BASE + 10
+    ADD_TODO = kp.ItemCategory.USER_BASE + 20
+
+    _todos = [
+            
+    ]
+
+
+    def __init__(self):
+        super().__init__()
+
+
+    def on_start(self):
+        #kp.live_package_dir # Find out where the live package dir is
+        #kp.shell_execute # Executes/opens file. Might be used to open the markdown-file
+        self._debug = True
+
+        self._todos = [
+            self.create_item(
+                category=self.TODO_CAT,
+                label="Fix 'Add Todo'",
+                short_desc="",
+                target="todo_1",
+                args_hint=kp.ItemArgsHint.FORBIDDEN,
+                hit_hint=kp.ItemHitHint.IGNORE,
+            ),
+            self.create_item(
+                category=self.TODO_CAT,
+                label="Fix 'Delete' Todo",
+                short_desc="It should be an action",
+                target="todo_2",
+                args_hint=kp.ItemArgsHint.FORBIDDEN,
+                hit_hint=kp.ItemHitHint.IGNORE,
+            ),
+            self.create_item(
+                category=self.TODO_CAT,
+                label="Add Finish action",
+                short_desc="",
+                target="todo_3",
+                args_hint=kp.ItemArgsHint.FORBIDDEN,
+                hit_hint=kp.ItemHitHint.IGNORE,
+            ),
+            self.create_item(
+                category=self.TODO_CAT,
+                label="Add a 'Assign to another' action",
+                short_desc="It will copy and delete",
+                target="todo_4",
+                args_hint=kp.ItemArgsHint.FORBIDDEN,
+                hit_hint=kp.ItemHitHint.IGNORE,
+            ),
+            self.create_item(
+                category=self.TODO_CAT,
+                label="What happens if they have the same target?",
+                short_desc="They need different targets otherwise they are not shown",
+                target="todo_5",
+                args_hint=kp.ItemArgsHint.FORBIDDEN,
+                hit_hint=kp.ItemHitHint.IGNORE,
+            ),
+            self.create_item(
+                category=self.TODO_CAT,
+                label="Test having a 'load more'",
+                short_desc="Do we want to encurage that behavior. Maybe a setting?",
+                target="todo_6",
+                args_hint=kp.ItemArgsHint.FORBIDDEN,
+                hit_hint=kp.ItemHitHint.IGNORE,
+            ),
+        ]
+
+    def on_catalog(self):
+        catalog = []
+
+        catalog.append(self.create_item(
+            category=kp.ItemCategory.KEYWORD,
+            label="Todo",
+            short_desc="Manages todos",
+            target="todo",
+            args_hint=kp.ItemArgsHint.REQUIRED,
+            hit_hint=kp.ItemHitHint.KEEPALL
+        ))
+
+        self.set_catalog(catalog)
+
+    def on_suggest(self, user_input, items_chain):
+        
+        if items_chain == self.TODO_CAT:
+            self.dbg("items_chain", items_chain[0])
+            self.dbg("user_input", user_input)
+            self.dbg("------------")
+
+
+        if not items_chain:
+            return
+        
+        suggestions = self._todos
+
+        if user_input:
+            target = user_input.strip().format(q = user_input.strip())
+            suggestions.append(
+                self.create_item(
+                    category=self.ADD_TODO,
+                    label = "Add '{}' as todo".format(user_input),
+                    short_desc=target,
+                    target=target,
+                    args_hint = kp.ItemArgsHint.FORBIDDEN,
+                    hit_hint = kp.ItemHitHint.IGNORE,
+                    loop_on_suggest = False
+                )
+            )
+            suggestions.append(
+                self.create_item(
+                    category=self.ADD_TODO,
+                    label = "Loop '{}' as todo".format(user_input),
+                    short_desc=target,
+                    target=target + "asd",
+                    args_hint = kp.ItemArgsHint.FORBIDDEN,
+                    hit_hint = kp.ItemHitHint.IGNORE,
+                    loop_on_suggest = True
+                )
+            )
+
+        self.set_suggestions(suggestions, kp.Match.DEFAULT, kp.Sort.NONE)
+
+    def on_execute(self, item, action):
+        pass
+
+    def on_activated(self):
+        pass
+
+    def on_deactivated(self):
+        pass
+
+    def on_events(self, flags):
+        pass
